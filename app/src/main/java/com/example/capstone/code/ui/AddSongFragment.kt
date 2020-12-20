4c0f1acc74ec44d8b56b8c7a04f02de9b16da919
ferran1 <ferrantl@live.nl>
2020-12-20 16:25:40 +0100

    Created the layout of add song fragment

--- app/src/main/java/com/example/capstone/code/ui/AddSongFragment.kt
+++ app/src/main/java/com/example/capstone/code/ui/AddSongFragment.kt
@@ -1,25 +1,45 @@
 package com.example.capstone.code.ui
 
 import android.os.Bundle
+import android.view.*
+import androidx.appcompat.widget.Toolbar
 import androidx.fragment.app.Fragment
-import android.view.LayoutInflater
-import android.view.View
-import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.navigation.fragment.findNavController
 import com.example.capstone.R
+import com.example.capstone.code.viewmodel.SongViewModel
+import com.example.capstone.databinding.FragmentAddSongBinding
 
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
 class AddSongFragment : Fragment() {
 
+    private lateinit var binding: FragmentAddSongBinding
+
+    private val viewModel: SongViewModel by viewModels()
+
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_add_song, container, false)
+        setHasOptionsMenu(true)
+
+        // Setup view binding to get the elements from the layout
+        binding = FragmentAddSongBinding.inflate(layoutInflater)
+        return binding.root
     }
 
+    override fun onCreateOptionsMenu(menu: Menu, inflater: MenuInflater) {
+        super.onCreateOptionsMenu(menu, inflater)
+        requireActivity().title = getString(R.string.add_song)
+
+        activity?.findViewById<Toolbar>(R.id.toolbar)!!.setNavigationOnClickListener {
+            findNavController().popBackStack()
+        }
+    }
+
+    // Setup the layout (view)
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
 
--- app/src/main/java/com/example/capstone/code/ui/SongBacklogFragment.kt
+++ app/src/main/java/com/example/capstone/code/ui/SongBacklogFragment.kt
@@ -3,23 +3,34 @@ package com.example.capstone.code.ui
 import android.os.Bundle
 import android.view.*
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.capstone.R
+import com.example.capstone.databinding.FragmentSongBacklogBinding
 
 /**
  * A simple [Fragment] subclass as the default destination in the navigation.
  */
 class SongBacklogFragment : Fragment() {
 
+    private lateinit var binding: FragmentSongBacklogBinding
+
     override fun onCreateView(
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View? {
-        // Inflate the layout for this fragment
-        return inflater.inflate(R.layout.fragment_song_backlog, container, false)
+        setHasOptionsMenu(true)
+
+        binding = FragmentSongBacklogBinding.inflate(layoutInflater)
+        return binding.root
     }
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+        requireActivity().title = getString(R.string.app_name)
+
+        binding.fabAddSong.setOnClickListener {
+            findNavController().navigate(R.id.action_songBacklogFragment_to_addSongFragment)
+        }
 
     }
 
