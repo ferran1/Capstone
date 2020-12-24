41853639640ea17c19f641e7ba84eb9cc7c90662
ferran1 <ferrantl@live.nl>
2020-12-24 21:31:06 +0100

    asynctask add youtube song, crawling doesn't work yet

--- app/src/main/java/com/example/capstone/code/ui/AddSongFragment.kt
+++ app/src/main/java/com/example/capstone/code/ui/AddSongFragment.kt
@@ -1,6 +1,9 @@
 package com.example.capstone.code.ui
 
+import android.annotation.SuppressLint
+import android.os.AsyncTask
 import android.os.Bundle
+import android.util.Log
 import android.view.*
 import android.widget.Toast
 import androidx.appcompat.widget.Toolbar
@@ -11,7 +14,6 @@ import com.example.capstone.R
 import com.example.capstone.code.viewmodel.SongViewModel
 import com.example.capstone.databinding.FragmentAddSongBinding
 
-
 /**
  * A simple [Fragment] subclass as the second destination in the navigation.
  */
@@ -21,9 +23,11 @@ class AddSongFragment : Fragment() {
 
     private val viewModel: SongViewModel by viewModels()
 
+    private lateinit var youtubeSongUrl: String
+
     override fun onCreateView(
-            inflater: LayoutInflater, container: ViewGroup?,
-            savedInstanceState: Bundle?
+        inflater: LayoutInflater, container: ViewGroup?,
+        savedInstanceState: Bundle?
     ): View? {
         setHasOptionsMenu(true)
 
@@ -53,30 +57,35 @@ class AddSongFragment : Fragment() {
 
     // Insert the song into the database
     private fun addSong() {
-        val songUrl = binding.etUrl.text.toString()
+        val songUrl: String = binding.etUrl.text.toString()
 
-        val songName = "Save Your Tears"
-        val artist = "The Weeknd"
-        val platform = "Youtube"
+        // Check from which platform
+        when {
+            songUrl.contains("youtube", ignoreCase = true) -> {
+                // If the URL is from Youtube, crawl the song name and artist from the Youtube in a background AsyncTask
+                youtubeSongUrl = songUrl // Save the url in a class variable because we want to later retrieve it in addYoutubeSong()
+                YoutubeAsyncTaskCrawler().execute(songUrl)
+            }
+            songUrl.contains("spotify", ignoreCase = true) -> {
+                getSongDataFromPlatform(songUrl, "spotify")
+            }
+            songUrl.contains("soundcloud", ignoreCase = true) -> {
+                getSongDataFromPlatform(songUrl, "soundcloud")
+            }
+            else -> {
+                Toast.makeText(context, getString(R.string.incorrect_url_msg), Toast.LENGTH_LONG)
+                    .show()
+            }
+        }
 
-        this.viewModel.insertSong(songUrl, songName, artist, platform)
-        findNavController().navigate(R.id.action_addSongFragment_to_songBacklogFragment)
 
-//        println(songUrl)
-//        Log.d("EA", songUrl)
+//        val songName = "Save Your Tears"
+//        val artist = "The Weeknd"
+//        val platform = "Youtube"
+//
+//        this.viewModel.insertSong(songUrl, songName, artist, platform)
+//        findNavController().navigate(R.id.action_addSongFragment_to_songBacklogFragment)
 
-
-            // Check from which platform
-//            if (songUrl.contains("youtube", ignoreCase = true)){
-//                getSongDataFromPlatform(songUrl, "youtube")
-//            } else if (songUrl.contains("spotify", ignoreCase = true)) {
-//                getSongDataFromPlatform(songUrl, "spotify")
-//            } else if (songUrl.contains("soundcloud", ignoreCase = true)){
-//                getSongDataFromPlatform(songUrl, "soundcloud")
-//            } else {
-//                Toast.makeText(context, getString(R.string.incorrect_url_msg), Toast.LENGTH_LONG)
-//                        .show()
-//            }
 
 // ------------------------------------------------------- song link examples:
         /*
@@ -97,7 +106,76 @@ class AddSongFragment : Fragment() {
 
     private fun getSongDataFromPlatform(url: String, platform: String) {
 
+    }
 
+    // Get the song name and artist from async task and add the Youtube song into the database
+    private fun addYoutubeSong(crawledNameAndArtist: String) {
+
+        // TODO:
+        // Split the crawdledNameAndArtist String into 2 strings, the artist and the song
+
+        this.viewModel.insertSong(youtubeSongUrl, crawledNameAndArtist, crawledNameAndArtist, "Youtube")
+
+        findNavController().navigate(R.id.action_addSongFragment_to_songBacklogFragment)
+
+//        Toast.makeText(
+//            context,
+//            "$crawledNameAndArtist Has successfully been scraped",
+//            Toast.LENGTH_LONG
+//        )
+//            .show()
 
     }
+
+    @SuppressLint("StaticFieldLeak")
+    inner class YoutubeAsyncTaskCrawler : AsyncTask<String, Void?, String>() {
+
+        override fun doInBackground(vararg params: String): String {
+
+            val url = params[0]
+//            val doc: Document = Jsoup.connect(params[0]).get()
+
+//            Log.d("test", url)
+
+            val crawledNameAndArtist = "Jason Derulo - Swag"
+
+            return crawledNameAndArtist
+        }
+
+
+        override fun onPostExecute(crawledNameAndArtist: String) {
+            super.onPostExecute(crawledNameAndArtist)
+            addYoutubeSong(crawledNameAndArtist)
+        }
+    }
 }
+
+// Web scraping example:
+
+//try {
+//    val url = "https://www.cinemaqatar.com/"
+//    val doc: Document = Jsoup.connect(url).get()
+//    val data: Elements = doc.select("span.thumbnail")
+//    val size: Int = data.size()
+//    Log.d("doc", "doc: $doc")
+//    Log.d("data", "data: $data")
+//    Log.d("size", "" + size)
+//    for (i in 0 until size) {
+//        val imgUrl: String = data.select("span.thumbnail")
+//            .select("img")
+//            .eq(i)
+//            .attr("src")
+//        val title: String = data.select("h4.gridminfotitle")
+//            .select("span")
+//            .eq(i)
+//            .text()
+//        val detailUrl: String = data.select("h4.gridminfotitle")
+//            .select("a")
+//            .eq(i)
+//            .attr("href")
+//        parseItems.add(ParseItem(imgUrl, title, detailUrl))
+//        Log.d("items", "img: $imgUrl . title: $title")
+//    }
+//} catch (e: IOException) {
+//    e.printStackTrace()
+//}
